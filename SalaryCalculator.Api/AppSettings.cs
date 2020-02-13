33b35f86dddee382451c2c948b039700a6a01afb
Fabian Grochla <fabian.grochla@student.po.edu.pl>
2020-02-13 21:49:52 +0100

    added depedency injecton to SalaryCalculatorController

---
+++ SalaryCalculator.Api/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using SalaryCalculator.SalaryReport;
+
+namespace SalaryCalculator.Api
+{
+    public class AppSettings
+    {
+        public MonthsWorkingHours MonthsWorkingHours { get; set; }
+        public CalculationsSummary CalculationsSummary { get; set; }
+        public Calculator Calculator { get; set; }
+    }
+}
--- SalaryCalculator.Api/Startup.cs
+++ SalaryCalculator.Api/Startup.cs
@@ -3,22 +3,35 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SalaryCalculator.SalaryReport;
+using System.Collections.Generic;
 
 namespace SalaryCalculator.Api
 {
     public class Startup
     {
+        private static readonly AppSettings _appSettings = new AppSettings();
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            //configuration.GetSection(key: "AppSettings").Bind(_appSettings);
+            var monthsWorkingHoursConfiguration =
+                JsonFileConverter.JsonFileConverter.ConvertFromFile<Dictionary<int, int>>
+                    ("MonthConfig.json");
+            _appSettings.MonthsWorkingHours = new MonthsWorkingHours(monthsWorkingHoursConfiguration);
+            _appSettings.CalculationsSummary = new CalculationsSummary();
+            _appSettings.Calculator = new Calculator(_appSettings.MonthsWorkingHours);
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddCors();
+            services.AddSingleton<MonthsWorkingHours>(_appSettings.MonthsWorkingHours);
+            services.AddSingleton<CalculationsSummary>(_appSettings.CalculationsSummary);
+            services.AddSingleton<Calculator>(_appSettings.Calculator);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -31,11 +44,8 @@ namespace SalaryCalculator.Api
                 .AllowAnyMethod()
                 .AllowAnyHeader());
             }
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
